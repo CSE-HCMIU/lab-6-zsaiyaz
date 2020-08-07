89fd6e574676fbcaa5dc31839faf47bf194799bf
ITDSIU19049 <69238956+zsaiyaz@users.noreply.github.com>
2020-08-07 16:31:21 +0700

    Update Exercise4.c

--- Exercise4.c
+++ Exercise4.c
@@ -13,6 +13,28 @@ Ex:
 
 int ex4(char *testcase){
 	//your codes here
+	   char blank[1000];
+   int c = 0, d = 0;
+   while (testcase[c] != '\0') {
+      if (testcase[c] == ' ') {
+         int temp = c + 1;
+         if (testcase[temp] != '\0') {
+            while (testcase[temp] == ' ' && testcase[temp] != '\0') {
+               if (testcase[temp] == ' ') {
+                  c++;
+               }  
+               temp++;
+            }
+         }
+      }
+      blank[d] = testcase[c];
+      c++;
+      d++;
+   }
+ 
+   blank[d] = '\0';
+ 
+   printf("testcase after removing blanks\n%s\n", blank);
 }
 
 int main(int argc, char *argv[]) {
