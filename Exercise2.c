7cb971c102591feaae4c941ed0e590b7a7d76bc0
ITDSIU19049 <69238956+zsaiyaz@users.noreply.github.com>
2020-08-07 17:43:01 +0700

    Update Exercise2.c

--- Exercise2.c
+++ Exercise2.c
@@ -17,12 +17,23 @@ int ex2(int n, char *str[]){
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int n = atoi(argv[1]);
-	char *str[n];
+	char str[25][25],temp[25];
 	int i;
 	for(i=0; i<n; i++){
 		str[i] = argv[i+2];
 	}
+            for(i=0;i<=n;i++)
+      for(int j=i+1;j<=n;j++){
+         if(strcmp(str[i],str[j])>0){
+            strcpy(temp,str[i]);
+            strcpy(str[i],str[j]);
+            strcpy(str[j],temp);
+         }
+      }
 
+   printf("Order of Sorted Strings:");
+   for(i=0;i<=n;i++)
+      puts(str[i]);
 	ex2(n, str);
 		
 	return 0;
