b8548dcef703cd73bc5b12fe14fdca901a35c3b3
ITDSIU19049 <69238956+zsaiyaz@users.noreply.github.com>
2020-08-07 17:26:11 +0700

    Update Exercise5.c

--- Exercise5.c
+++ Exercise5.c
@@ -17,10 +17,10 @@ Ex:
 int i,top = -1;
 char stack[100];
 int ex5(char *testcase){
-		for (i = 0; testcase[i] != '\0';i++)
+	for (i = 0; testcase[i] != '\0';i++)
 	{
-		if (testcase[i] == '(')
-		{
+	if (testcase[i] == '(')
+	{
 			stack[top] = testcase;
 	        top++;
 		}
@@ -28,7 +28,7 @@ int ex5(char *testcase){
 		{
 			if (top == -1)
         	{
-	        	printf(" is invalid\n");
+	        	printf("invalid\n");
 		        exit(0);
 	        }	
 	        else
@@ -46,10 +46,10 @@ int ex5(char *testcase){
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
-	char *testcase1="[{()}]";
-    char *testcase2="([)]";
+    char *testcase1 ="[{()}]";
+    char *testcase2 ="([)]";
 
-	ex5(testcase1);
+    ex5(testcase1);
     ex5(testcase2);
 		
 	return 0;
